fb66d32a77a2bad1cf1f2f3727bfe340324fd88b
James Tombleson <luther38@gmail.com>
2019-02-15 06:30:50 -0800

    updated some sql

--- src/PowerShell.js
+++ src/PowerShell.js
@@ -2,6 +2,8 @@
 const Shell = require('node-powershell');
 const path = require('path');
 const fs = require('fs');
+const uuid = require('uuid/v4');
+const sqlTasks = require('./sqlite/tasks');
 
 function runScript(PathScript, Args, logPath ){
     let ps = new Shell({
@@ -9,18 +11,15 @@ function runScript(PathScript, Args, logPath ){
         noProfile: true
     });
     
-    console.log(Args);
-    
-    let ArgsArray = [];
-    let a = Object.keys(Args);
-    a.forEach(key =>{
-        if(Args[key] != ""){
-            //let s = `{${key}: ${Args[key]}},`;
-            let s = `${key} ${Args[key]}`;
-            ArgsArray.push(s);
-        }
+    // Process the args and replace what we got so we can use it with PS
+    ParseArgs(Args, function (ArgsArray) {
+        Args = ArgsArray;
     });
     
+    // Generate a new ID for SQL
+    let guid = uuid();
+
+    sqlTasks.Insert()
 
     if( Args == undefined){
         //let p = path.resolve(PathScript)
@@ -31,16 +30,32 @@ function runScript(PathScript, Args, logPath ){
     }
 
     ps.invoke()
-    .then(output =>{
-        fs.appendFile(logPath, '\n'+output, (err)=>{
-            if(err) throw err;
-            console.log(output);
-        });      
+    .then(output => {
+        // Update the record that we finished
+
+        // Load the results we got back into logs with the guid
     })
     .catch(err => { 
         console.log(err);
         ps.dispose();
     })
+}
+
+function ParseArgs(Args, Callback){
+    let ArgsArray = [];
+    let a = Object.keys(Args);
+    a.forEach(key =>{
+        if(Args[key] != ""){
+            //let s = `{${key}: ${Args[key]}},`;
+            let s = `${key} ${Args[key]}`;
+            ArgsArray.push(s);
+        }
+    });
+    return Callback(ArgsArray);
+}
+
+function InsertIntoTasks() {
+
 }
 
 module.exports.runScript = runScript;
--- src/sqlite/tasks.js
+++ src/sqlite/tasks.js
@@ -1,22 +1,38 @@
 let sql = require('sqlite3').verbose();
 let db = new sql.Database("db.sqlite");
 
-function Select(){
+function SelectAll(callback) {
     db.all("select * from 'tasks' where Status = 'Active'", function (err, rows) {
-        if(err) {
-            console.error(err);
-        }
-        
-        return rows;
+        if(err) { return callback(err); }
+        return callback(null, rows);
+        //return rows;
     });
 }
 
-function Insert(Name, Status, StartTime,FinishTime){
-    db.run("insert into tasks ( Name, Status, StartTime,FinishTime,LogData) Values ( 'HelloWorld', 'Active', '02/14/2019 03:01:00 PM', '', 1)")
+function SelectWhere(callback){
+    db.all("select * from 'tasks' where Status = 'Active'", function (err, rows) {
+        if(err) { callback(err); }        
+        return callback(null,rows);
+    });
+}
+
+function Insert(Name, Status, StartTime,FinishTime, callback){
+    db.run("insert into tasks ( Name, Status, StartTime,FinishTime,LogData) Values ( 'HelloWorld', 'Active', '02/14/2019 03:01:00 PM', '', 1)", function (err, result) {
+       if (err) {return callback(err);}
+       return callback(null, result);
+    });
 }
 
 function Update(ID){
-    db.run("insert into tasks ( Name, Status, StartTime,FinishTime,LogData) Values ( 'HelloWorld', 'Active', '02/14/2019 03:01:00 PM', '', 1)")
+    db.run("insert into tasks ( Name, Status, StartTime,FinishTime,LogData) Values ( 'HelloWorld', 'Active', '02/14/2019 03:01:00 PM', '', 1)", function (err, result) {
+        if (err) {return callback(err);}
+        return callback(null, result);
+    })
 }
 
-module.exports = {Select};
+module.exports = { 
+    SelectAll, 
+    SelectWhere, 
+    Insert, 
+    Update
+};
